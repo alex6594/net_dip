106e79d4e7d2f9bee21f8d4579721266a83cd86c
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-10-16 21:43:30 +0100

    changed name mysite to myclolud back

--- mycloud/settings.py
+++ mycloud/settings.py
@@ -160,6 +160,7 @@ MAX_AVATAR_SIZE = 3145728
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'http://127.0.0.1:8000/login',
     'https://netdipalex-production.up.railway.app',
 ]
 
