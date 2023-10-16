5fd3e1a910530a94f9313374a3edc3c30dd3e38a
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-10-16 17:19:30 +0100

    Update settings.py

--- mycloud/settings.py
+++ mycloud/settings.py
@@ -138,10 +138,10 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'static'
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 MEDIA_URL = '/media/'
