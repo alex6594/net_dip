271a5793e3ed16cf73ae8d5b258f77b7fd89c30a
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-10-17 10:50:05 +0100

    change: settings.py

--- mycloud/settings.py
+++ mycloud/settings.py
@@ -158,14 +158,14 @@ MAX_AVATAR_SIZE = 3145728
 
 # Cross origin
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',
-    'http://127.0.0.1:8000',
+    # 'http://localhost:8000',
+    # 'http://127.0.0.1:8000',
     'https://netdipalex-production.up.railway.app',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://netdipalex-production.up.railway.app',
-    'http://127.0.0.1:8000',
+    # 'http://127.0.0.1:8000',
 ]
 
 # Rest framework
